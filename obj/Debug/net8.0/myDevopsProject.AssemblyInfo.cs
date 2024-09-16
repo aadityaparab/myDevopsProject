5a98570c21362012f2bb4d42e0f9bd7ef1f1dc55
Aaditya Parab <parab1997@gmail.com>
2024-09-16 20:29:00 +0530

    updated welcome page.

--- obj/Debug/net8.0/myDevopsProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/myDevopsProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myDevopsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15d83dab859988072e5d9f3b935c864c6df02901")]
 [assembly: System.Reflection.AssemblyProductAttribute("myDevopsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myDevopsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
